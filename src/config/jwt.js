5a8b72413f47e420a4c7bf76275d53556734d7b5
OmarSalahBot <Omarsalah8001@gmail.com>
2026-05-05 04:35:40 +0300

    fix/jwt-edit

--- src/config/jwt.js
+++ src/config/jwt.js
@@ -7,6 +7,7 @@ export const generateToken = (userId , res) => {
     
     res.cookie("jwt" , token , {
         maxAge: 7 * 24 * 60 * 60 * 1000 ,
+        secure: true,
         httpOnly : true, // Prevent XSS attacks : cross side-scripting
         sameSite:"strict", // CSRF Attacks
         secure: process.env.NODE_ENV == "development" ? false : true
